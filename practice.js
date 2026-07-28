ac2c474b02f1e794a6b5bf01617548fb6cfb2bc0
Clifford <gudaclifford@gmail.com>
2026-07-28 15:17:05 +0800

    Update practice.js

--- practice.js
+++ practice.js
@@ -968,3 +968,66 @@ This is useful when you want to store or transmit data in a format that can be e
 // let { name, age, country = "Unknown" } = person;
 
 // console.log(country); // Unknown
+
+
+//ARRAY AND OBJECT LAB ACTIVITY
+// const recipes = [];
+
+// const recipe1 = {
+//   name: "Spaghetti Carbonara",
+//   ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+//   cookingTime: 22,
+//   totalIngredients: null,
+//   difficultyLevel: ""
+// };
+
+// const recipe2 = {
+//   name: "Chicken Curry",
+//   ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+//   cookingTime: 42,
+//   totalIngredients: null,
+//   difficultyLevel: ""
+// };
+
+// const recipe3 = {
+//   name: "Vegetable Stir Fry",
+//   ingredients: ["broccoli", "carrot", "bell pepper"],
+//   cookingTime: 15,
+//   totalIngredients: null,
+//   difficultyLevel: ""
+// };
+
+// recipes.push(recipe1, recipe2, recipe3); // Push all object to ARRAY
+
+// function getTotalIngredients(ingredients) {
+//   return ingredients.length; // Check how many ingredients in number
+// }
+
+// function getDifficultyLevel(cookingTime) {
+//   if (cookingTime <= 30) {
+//     return "easy";
+//   } else if (cookingTime <= 60) {
+//     return "medium";
+//   } else if(cookingTime => 60) {
+//     return "hard";
+//   }
+// }
+
+// // log specific info from recipe1
+// const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+// console.log(recipe1TotalIngredients);
+
+// const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+// console.log(recipe1DifficultyLevel);
+
+// //UPDATE the object, update the null and empty string 
+// recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+// recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+
+// recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+// recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+
+// recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients);
+// recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime);
+
+// console.log(recipes);
