273a7ab719594cad0ce35858a009819ede1f765f
Clifford <gudaclifford@gmail.com>
2026-08-01 15:37:44 +0800

    Update proc.js

--- proc.js
+++ proc.js
@@ -43,6 +43,61 @@
 
 
 // STRINGS
-const text = "Welcome to wild rift, summoners!";
-console.log(text.length);
-console.log(typeof text);
+// const text = "Welcome to wild rift, summoners!";
+// console.log(text.length);
+// console.log(typeof text);
+// console.log(text.includes("wild"));
+// console.log(text.slice(11,20));
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.replace("summoners","Kenny"));
+
+//LOGICAL OPERATORS 
+// const a = 5;
+// const b = 10;
+// const c = 1;
+// const d = 9;
+
+// console.log(a > b);
+// console.log(b > a);
+// console.log(a <= b);
+// console.log(d >= c);
+// console.log(c === a);
+
+
+//BOOLEAN
+// const status = {
+//     isAdmin: false,
+//     isLogin: true,
+//     isCustomer: false
+// };
+
+// function statusAccess(status) {
+//     if (status.isAdmin) {
+//         console.log("Welcome Admin");
+//     } else if (status.isLogin) {
+//         console.log("Welcome back, Employee");
+//     } else if (status.isCustomer) {
+//         console.log("Welcome Customer");
+//     } else {
+//         console.log("Access Denied");
+//     }
+// }
+// console.log(status);
+// statusAccess(status);
+
+
+
+//ATM SIMULATOR 
+const account = {
+    accountName: "kenny",
+    accountId: 111,
+    balance: 100,
+    pin: 'ac001',
+    isLocked: true        
+};
+console.log(account);
+
+function deposit(amount){
+    
+}
