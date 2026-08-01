aec1b2fd928a6b1d161f92dc7fd2bbc3981f76ab
Clifford <gudaclifford@gmail.com>
2026-08-01 14:47:19 +0800

    Add files via upload

---
+++ proc.js
@@ -0,0 +1,48 @@
+//STUDENT PROFILE 
+// const student = {
+//     name: "Kenny",
+//     age: 20,
+//     course: "BSIT",
+//     studentId: 1100,
+//     gpa: 1.50,
+//     enrolled: true
+// };
+// console.log(student);
+
+// function changeCourse(student,newCourse){
+//     return student.course = newCourse;
+// };
+// changeCourse(student, "CE");
+// console.log(student);
+
+// function updateGpa(student,newGpa){
+//     return student.gpa = newGpa;
+// };
+// updateGpa(student, 2.00);
+// console.log(student);
+
+// function isEnrolled(student){
+//     if(student.enrolled){
+//         console.log("is enrolled");
+//     } else{
+//         console.log("droped");
+//     }
+// };
+// isEnrolled(student);
+
+
+// CALCULATOR
+// let num1 = 5;
+// let num2 = 10;
+
+// let result = num1 * num2;
+// result = num1 - num2;
+// result = num2 / num1;
+
+// console.log(result);
+
+
+// STRINGS
+const text = "Welcome to wild rift, summoners!";
+console.log(text.length);
+console.log(typeof text);
