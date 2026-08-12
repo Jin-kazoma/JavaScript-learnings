f3b10471734f739a5a512f17eef69f165771b120
Clifford <gudaclifford@gmail.com>
2026-08-12 15:03:57 +0800

    Add files via upload

---
+++ dom project/script.js
@@ -0,0 +1,7 @@
+const name = document.getElementById("name");
+console.log(name);
+name.textContent = "ken";
+
+const course = document.querySelector("course");
+console.log(course);
+course.textContent = "BSCS";
