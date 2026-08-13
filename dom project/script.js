b2e5000bfca004d6248177e5d53e31ca8e6cd4a2
Clifford <gudaclifford@gmail.com>
2026-08-13 13:37:22 +0800

    Update script.js

--- dom project/script.js
+++ dom project/script.js
@@ -81,3 +81,49 @@
 // for(let i = 0; i < food.length; i++){
 //     console.log(`Order: ${food[i].value}`);
 // };
+
+
+
+// ADDEVENTLISTENER EXERCISE
+// const input = document.getElementById("input");
+// input.addEventListener("input", () => {
+//     console.log(input.value);
+// })
+
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", () => alert ("Submitted"));
+
+// const form = document.querySelector("#myForm");
+
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();// used to stop refreash of page and recover the info
+
+//     console.log("Form submitted without refreshing!");
+// });
+
+//2.2 
+// const form = document.querySelector("#profileForm");
+// const username = document.querySelector("#username");
+// const course = document.querySelector("#course");
+// const button = document.querySelector("button")
+// const preview = document.querySelector("#preview");
+// const message = document.querySelector("#message");
+
+// username.addEventListener("input", function(event){
+//     console.log(username.value);
+//     preview.textContent = username.value;
+// });
+
+// course.addEventListener("change", function(event){
+//     console.log(course.value);
+// });
+
+// form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     message.textContent = "Profile Submitted!";
+// });
+
+// button.addEventListener("click", function(event){
+//     console.log("Button Clicked!");
+//     console.log(event.target);
+// });
